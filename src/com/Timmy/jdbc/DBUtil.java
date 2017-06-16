3cd27dffd7102a20d2dcd37838777cae6818dfc2
Raphealkxy <1277609958@qq.com>
2017-06-16 17:42:43 +0800

    add project

---
+++ src/com/Timmy/jdbc/DBUtil.java
@@ -0,0 +1,33 @@
+package com.Timmy.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBUtil {
+	 private static final String url="jdbc:mysql://127.0.0.1:3306/imooc?characterEncoding=utf8&useSSL=false";
+	 private static final String name="root";
+	 private static final String password="kxy1277609958";
+
+	   
+	public static void main(String[] args) throws Exception {
+		
+	    
+		 //1.加载数据库驱动
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		
+		   //2.连接数据库
+		Connection connection=DriverManager.getConnection(url, name, password);
+		
+		   //3.创建statement对象,通过数据库的连接实现数据的增删改查
+		Statement statement=connection.createStatement();
+		ResultSet set=statement.executeQuery("select user_name,age from imoocgoddes");
+		while(set.next())
+		{
+			System.out.println(set.getString("user_name")+","+set.getInt("age"));
+		}
+	}
+	   
+}
