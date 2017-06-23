6c61951679ec39c56956ab7a4d3d771d9e41b499
Raphealkxy <1277609958@qq.com>
2017-06-23 19:18:49 +0800

    modify the query and add del

--- src/com/Timmy/action/java_action.java
+++ src/com/Timmy/action/java_action.java
@@ -1,8 +1,11 @@
 package com.Timmy.action;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.Timmy.model.Goddness;
 import com.Timmy.model.GoddnessDuo;
@@ -25,9 +28,21 @@ public class java_action {
             //goddnessDuo.updateGodness("小美");
             //测试get
             //Goddness g=goddnessDuo.get("小美");
-            List<Goddness> reList=goddnessDuo.query();
-            for(int i=0;i<reList.size();i++)
-            System.out.println("姓名:"+reList.get(i).getUser_name()+" "+"性别:"+reList.get(i).getSex()+" "+"年龄"+reList.get(i).getAge());
+//            List<Goddness> reList=goddnessDuo.query();
+//            for(int i=0;i<reList.size();i++)
+//            System.out.println("姓名:"+reList.get(i).getUser_name()+" "+"性别:"+reList.get(i).getSex()+" "+"年龄"+reList.get(i).getAge());
+           //  goddnessDuo.Del("小美");    
+            //测试映射JDBC
+            List<Map<String, Object>>pList=new ArrayList<Map<String,Object>>();
+            Map<String,Object>map=new HashMap<String, Object> ();
+            map.put("name", "user_name");
+            map.put("rel", "=");
+            map.put("value", "'小明'");//注意字符串要加单引号来区分值为字符
+            pList.add(map);
+            List<Goddness> reList=goddnessDuo.query(pList);
+          for(int i=0;i<reList.size();i++)
+          System.out.println("姓名:"+reList.get(i).getUser_name()+" "+"性别:"+reList.get(i).getSex()+" "+"年龄"+reList.get(i).getAge());
+            
             
 	}
 }
--- src/com/Timmy/model/GoddnessDuo.java
+++ src/com/Timmy/model/GoddnessDuo.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.Timmy.jdbc.DBUtil;
 
@@ -68,6 +69,39 @@ public class GoddnessDuo {
     	//PreparedStatement ptmt
     	return result;
     }
+    //对查询进行更细节的优化
+    public List<Goddness> query(List<Map<String, Object>>paras) throws SQLException
+    {
+    	List<Goddness> result = new ArrayList<Goddness>();
+    	Goddness goddness =null;
+    	String sqlString="";
+    	sqlString="select User_name,Age,Sex from imoocgoddes where 1=1 ";//对条件进行的定制化 name 关系 值
+		if (paras != null && paras.size() > 0)
+			for (int i = 0; i < paras.size(); i++) {
+				//System.out.println(1);
+				Map<String, Object> map = paras.get(i);
+				sqlString = sqlString + " and " + map.get("name")
+						+ map.get("rel") + map.get("value")+" ";
+			}
+		//System.out.println(sqlString);
+    	Connection connection=null;
+    	connection=DBUtil.connect();
+    	PreparedStatement ptmp=connection.prepareStatement(sqlString);
+    	ResultSet rSet=ptmp.executeQuery();
+    	while(rSet.next())
+		{
+    		goddness=new Goddness();
+			goddness.setUser_name(rSet.getString("user_name"));
+			goddness.setAge(rSet.getInt("age"));
+			goddness.setSex(rSet.getInt("sex"));
+			result.add(goddness);
+		}
+    	
+    	
+    	
+    	//PreparedStatement ptmt
+    	return result;
+    }
     
     public Goddness get(String name) throws SQLException//本模块只提供按名字获取信息
     {
@@ -88,6 +122,18 @@ public class GoddnessDuo {
         
         
     	return goddness;
+    }
+    
+    public void Del(String name) throws SQLException
+    {
+    	Connection connection=null;
+    	String sql="";
+    	sql=" Delete from imoocgoddes where user_name=?";
+    	connection=DBUtil.connect();
+    	PreparedStatement ptmt=connection.prepareStatement(sql);
+    	ptmt.setString(1, name);
+    	ptmt.execute();
+    	
     }
     
     
